fb174beb50900de7a944815c10f13ae29114eb66
GutImozih73 <ilmirayagudina1996@gmail.com>
2023-06-19 09:49:00 +0300

    fix: удалить создание обекта хистори в методе сейфхистор

--- src/taskmanager/FileBackedTasksManager.java
+++ src/taskmanager/FileBackedTasksManager.java
@@ -58,7 +58,6 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
     }
 
     protected void saveToHistory(Task task) {
-        HistoryManager historyManager = new InMemoryHistoryManager();
         historyManager.add(task);
     }
 
